7ac238b83f22711ad77413dbe74bed5c394e990e
Jitender <yadavjitender805@gmail.com>
2024-10-18 12:56:02 +0530

    yett

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let Songs = [
+let songs = [
+    {
+        h2: "Opinion",
+        image: "assets/manifest.webp",
+        song: "assets/Opinion.mp3"
+    },
     {
         h2: "Score",
         image: "assets/awara.jpg",
         song: "assets/Score.mp3"
+    },
+    {
+        h2: "Woah",
+        image: "assets/Chobar.jpg",
+        song: "assets/Woah.mp3"
     },
     {
         h2: "Calculations",
@@ -15,11 +25,6 @@ let Songs = [
         song: "assets/Munde Pindan De.mp3"
     },
     {
-        h2: "Opinion",
-        image: "assets/manifest.webp",
-        song: "assets/Opinion.mp3"
-    },
-    {
         h2: "Panjabi",
         image: "assets/Saroor.jpg",
         song: "assets/Panjabi.mp3"
@@ -29,12 +34,8 @@ let Songs = [
         image: "assets/Chobar.jpg",
         song: "assets/Youth_Flow .mp3"
     },
-    {
-        h2: "Woah",
-        image: "assets/Chobar.jpg",
-        song: "assets/Woah.mp3"
-    },
 ];
+
 
 
 let h2 = document.querySelector("#title");
@@ -47,11 +48,34 @@ let forwardbtn = document.getElementById("forward");
 let reversebtn = document.getElementById("reverse");
 let audio = document.getElementById("audio");
 
+
+let progress_container = document.getElementById("progress-container");
+let progress = document.getElementById("progress");
+let audio_current_time = document.getElementById("audio_current_time");
+let audio_duration = document.getElementById("audio_duration");
+let shuffle_btn = document.getElementById("shuffle");
+let repeat_all_btn = document.getElementById("repeat_all");
+
+let isShuffle = false;
+let isRepeatAll = false
+let originalSongs = [...songs];
+
+
+function updateSongDetails() {
+    songName.innerHTML = songs[position].title;
+    songDesc.innerHTML = songs[position].genre;
+    songImg.src = songs[pos].img;
+    audio.src = songs[pos].src;
+}
+
 //Play Function
-audio.setAttribute('src', Songs[0].song)
-ImageTag.src = Songs[0].image;
-h2.innerHTML = Songs[0].h2;
+audio.setAttribute('src', songs[0].song)
+ImageTag.src = songs[0].image;
+h2.innerHTML = songs[0].h2;
 isPlaying = 0;
+
+// playbtn
+let position =0;
 
 function play() {
     if (playbtn.classList.contains('bi-play-circle-fill')) {
@@ -69,52 +93,158 @@ playbtn.addEventListener('click', play)
 
 
 
-// songLength 
-let position = 1;
-songLength.innerHTML = position + "/" + Songs.length;
-
-
 
 //forward btn System
-position = 0;
 forwardbtn.addEventListener('click', () => {
-    if (Songs.length <= position + 1) {
+    if (songs.length <= position + 1) {
         position = 0;
     } else {
         position++;
     }
-    audio.src = Songs[position].song;
-    ImageTag.src = Songs[position].image;
-    h2.innerHTML = Songs[position].h2;
+    audio.src = songs[position].song;
+    ImageTag.src = songs[position].image;
+    h2.innerHTML = songs[position].h2;
 
     // songLength 
-    songLength.innerHTML = (position + 1) + "/" + Songs.length;
+    songLength.innerHTML = (position + 1) + "/" + songs.length;
 
     audio.play();
 });
 
 //reverse btn System
-
-position = 0;
 reversebtn.addEventListener('click', () => {
     if (position == 0) {
-        position = Songs.length - 1;
+        position = songs.length - 1;
     } else {
         position--;
     }
-    audio.src = Songs[position].song;
-    ImageTag.src = Songs[position].image;
-    h2.innerHTML = Songs[position].h2;
+    audio.src = songs[position].song;
+    ImageTag.src = songs[position].image;
+    h2.innerHTML = songs[position].h2;
 
     // songLength 
-    songLength.innerHTML = (position + 1) + "/" + Songs.length;
+    songLength.innerHTML = (position + 1) + "/" + songs.length;
 
     audio.play();
 
 });
 
-    
-    audio.addEventListener("timeupadte",()=>{
-        let{ duration ,currentTime} =audio;
-        console.log(duration,currentTime);
-    });
+
+// progress bar
+
+
+
+// let updateProgress = () => {
+//     let { duration, currentTime } = audio;
+//     let progressPercent = (currentTime / duration) * 100;
+//     progress.style.width = `${progressPercent}%`;
+//     audio_current_time.innerHTML = sToTime(currentTime);
+//     audio_duration.innerHTML = isNaN(duration) ? "00:00" : sToTime(duration);
+//   };
+  
+//   let setProgress = (e) => {
+//     let width = progress_container.clientWidth;
+//     let clickX = e.offsetX;
+//     let duration = audio.duration;
+//     audio.currentTime = (clickX / width) * duration;
+//   };
+  
+//   function sToTime(t) {
+
+// updateSongDetails();
+// audio.addEventListener("timeupdate", updateProgress);
+// shuffle_btn.addEventListener("click", shuffle);
+// progress_container.addEventListener("click", setProgress);
+// audio.addEventListener("ended", next);
+// audio_pause_btn.addEventListener("click", () => {
+//   if (audio.paused) {
+//     audio.play();
+//     audio_pause_btn.children[0].className = "bi bi-pause-fill";
+//   } else {
+//     audio.pause();
+//     audio_pause_btn.children[0].className = "bi bi-play-fill";
+//   }
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   audio.play();
+// });
+//     return padZero(parseInt((t / 60) % 60)) + ":" + padZero(parseInt(t % 60));
+//   }
+  
+//   function padZero(v) {
+//     return v < 10 ? "0" + v : v;
+//   }
+
+
+//   shuffle btn
+
+
+// let shuffle = () => {
+//     if (!isShuffle) {
+//       shuffle_btn.childNodes[0].classList.add("text-primary");
+//       shuffleSongs(songs); 
+//       isShuffle = true;
+//     } else {
+//       shuffle_btn.childNodes[0].classList.remove("text-primary");
+//       songs = [...originalSongs]; 
+//       isShuffle = false;
+//     }
+//     pos = 0;
+//     updateSongDetails();
+//     audio.play();
+//     audio_pause_btn.children[0].className = "bi bi-pause-fill";
+//   };
+
+
+//   repeat btn
+
+
+
+// let repeatAll = () => {
+//     if (!isRepeatAll) {
+//       repeat_all_btn.childNodes[0].classList.add("text-primary");
+//       isRepeatAll = true;
+//       audio.play();
+//       audio_pause_btn.children[0].className = "bi bi-pause-fill";
+//       audio.addEventListener("ended", next);
+//     } else {
+//       repeat_all_btn.childNodes[0].classList.remove("text-primary");
+//       isRepeatAll = false;
+//       audio.addEventListener("ended", ()=>{
+//         audio.pause();
+//         audio_pause_btn.children[0].className = "bi bi-play-fill";
+//       })
+//     } 
+//   };
+
+
+
+
+// audio.addEventListener("timeupadte", () => {
+//     let { duration, currentTime } = audio;
+//     console.log(duration, currentTime);
+// });
+
+
+
+
+
+// updateSongDetails();
+// audio.addEventListener("timeupdate", updateProgress);
+// shuffle_btn.addEventListener("click", shuffle);
+// progress_container.addEventListener("click", setProgress);
+// audio.addEventListener("ended", next);
+// audio_pause_btn.addEventListener("click", () => {
+//   if (audio.paused) {
+//     audio.play();
+//     audio_pause_btn.children[0].className = "bi bi-pause-fill";
+//   } else {
+//     audio.pause();
+//     audio_pause_btn.children[0].className = "bi bi-play-fill";
+//   }
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   audio.play();
+// });
